a9e6353bc43ef8f8ce45a41c6b6d88b31364d255
[Yana] <janakoo@yandex.ru>
2022-04-12 21:05:14 +0300

    2_5 new code

--- 2_5.py
+++ 2_5.py
@@ -1,18 +1,20 @@
 my_list = [110, 18, 15, 13, 13, 12, 1]
 print("Набор чисел:", my_list)
-input_value = int(input('Введите натуральное число: '))
-
+input_value = float(input('Введите натуральное число: '))
+i = 0
 if input_value < 1:
     print(input_value, " не является натуральным числом")
 else:
     length = len(my_list)
     if my_list[0] < input_value:
         my_list.insert(0, input_value)
-    elif my_list[length - 1] > input_value:
+    elif my_list[length - 1] >= input_value:
         my_list.append(input_value)
     else:
-        for i in range(length - 1):
-            if my_list[i] > input_value and my_list[i + 1] <= input_value:
+        while i < length - 1:
+            if my_list[i] >= input_value and my_list[i + 1] < input_value:
                 my_list.insert(i + 1, input_value)
+                break
+            i = i + 1
 
     print("Новый набор чисел:", my_list)
